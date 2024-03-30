80945343c8c173e4ab73272856448c8ea9e04248
vaageeshadas <vaageesha.das@gmail.com>
2024-03-30 11:42:11 -0400

    setting up gpt woo hoo

--- wiki.py
+++ wiki.py
@@ -1,21 +1,40 @@
 import wikipediaapi
-import openai
+# import openai
+from openai import OpenAI
+
+
 
 
 def prep_openai(api_key, org_id):
-    openai.api_key = api_key
-    openai.organization = org_id
+    OpenAI.api_key = api_key
+    OpenAI.organization = org_id
     
 
-def promp_GPT(prompt):
-    completion = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
-        )
+# def promp_GPT(prompt):
+#     completion = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ]
+#     )
+#     return completion.choices[0].message["content"]
+
+
+def promp_GPT(prompt, api_key):
+    client = OpenAI(api_key=api_key)
     
-    return completion.choices[0].message.content
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ],
+)
+    print(completion.choices[0].message.content)
+
 
 
 def fetch_history(country):
@@ -34,8 +53,11 @@ def fetch_history(country):
 
 country_name = "Japan"
 history_text = fetch_history(country_name)
-# print(history_text)
-prep_openai("key", "org_id")
+print(history_text[:8000])
+print()
+print("--------------------------")
+print()
 
-prompt = "find and summarize relating to x, y, z" + history_text
+prompt = "can you summarize this:" + history_text
+print(promp_GPT(prompt, API_KEY_HERE))
                 
