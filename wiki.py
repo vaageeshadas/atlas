ed471a5925c558febcb0dc6c58dc6ef282d79786
vaageeshadas <vaageesha.das@gmail.com>
2024-03-29 23:03:22 -0400

    added stuff for using api key

--- wiki.py
+++ wiki.py
@@ -1,4 +1,22 @@
 import wikipediaapi
+import openai
+
+
+def prep_openai(api_key, org_id):
+    openai.api_key = api_key
+    openai.organization = org_id
+    
+
+def promp_GPT(prompt):
+    completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+    
+    return completion.choices[0].message.content
+
 
 def fetch_history(country):
     wiki_wiki = wikipediaapi.Wikipedia(
@@ -16,4 +34,8 @@ def fetch_history(country):
 
 country_name = "Japan"
 history_text = fetch_history(country_name)
-print(history_text)
+# print(history_text)
+prep_openai("key", "org_id")
+
+prompt = "find and summarize relating to x, y, z" + history_text
+                
