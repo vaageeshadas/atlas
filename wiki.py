c40411750d5de8c87215656c4dbb49cfd55b4c44
vaageeshadas <vaageesha.das@gmail.com>
2024-03-30 12:27:49 -0400

    extracting relevant year text

--- wiki.py
+++ wiki.py
@@ -1,24 +1,11 @@
 import wikipediaapi
-# import openai
 from openai import OpenAI
-
-
 
 
 def prep_openai(api_key, org_id):
     OpenAI.api_key = api_key
     OpenAI.organization = org_id
     
-
-# def promp_GPT(prompt):
-#     completion = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=[
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": prompt}
-#         ]
-#     )
-#     return completion.choices[0].message["content"]
 
 
 def promp_GPT(prompt, api_key):
@@ -36,6 +23,14 @@ def promp_GPT(prompt, api_key):
     print(completion.choices[0].message.content)
 
 
+def extract_years(content, start_year, end_year):
+    relevant_content = []
+    for line in content.split('\n'):
+        if any(str(year) in line for year in range(start_year, end_year + 1)):
+            relevant_content.append(line)
+    return '\n'.join(relevant_content)
+
+
 
 def fetch_history(country):
     wiki_wiki = wikipediaapi.Wikipedia(
@@ -51,13 +46,19 @@ def fetch_history(country):
     return p_wiki.text
 
 
+
 country_name = "Japan"
 history_text = fetch_history(country_name)
-print(history_text[:8000])
-print()
-print("--------------------------")
+# print(history_text[:8000])
+# print()
+# print("--------------------------")
+# print()
+
+new_text = extract_years(history_text, 1800, 1900)
+print(new_text)
 print()
 
-prompt = "can you summarize this:" + history_text
-print(promp_GPT(prompt, API_KEY_HERE))
+
+# prompt = "can you summarize what is happening during the 1800s:" + new_text
+# promp_GPT(prompt, "API_KEY_HERE")
                 
