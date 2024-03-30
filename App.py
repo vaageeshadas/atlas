b6fe6c2ed095764e04e46c4b941072b1285606bc
dhiya r. <dhiyayr@gmail.com>
2024-03-29 20:12:01 -0400

    added app.py

---
+++ App.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify, abort, session
+import os
+from pathlib import Path
+from sys import path
+import json
+from urllib.parse import urlparse, parse_qs
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5007, debug = False)
